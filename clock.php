09042ce11783a5fa9765cb68a1e85684904e5b43
viktori57 <vrenezi.viktor57@gmail.com>
2023-09-12 16:29:23 +0200

    Commit all

---
+++ clock.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Horloge</title>
+    <head>
+        <body>
+      <?<php>
+        <time>
+        </time>
+      </php?>      
+
+
+
+
+
+
+
+        </body>
+    </head>
