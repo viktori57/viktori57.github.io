406dc064e3361a4d28d56565433acbbc17db3600
viktori57 <vrenezi.viktor57@gmail.com>
2023-09-18 16:18:19 +0200

    Commit all

---
+++ cours_presentielle/10.code_retrait/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width=, initial-scale=1.0">
+    <title>code_retrait</title>
+</head>
+<body>
+    <div class="contenair"></div>
+        <div class="code"></div>
+        <label for="password"></label>
+        <input type="password" name="password id="password">
+        <div class="LittleContenair"></div>
+        <div class="button">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button type="reset">Decliner</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button type="reset">Effacer</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button type="submit">Entrez</button>
+            <button>-</button>
+            <button>0</button>
+            <button>+</button>
+        </div>
+
+            
+   
+    
+   
+    
+
+
+
+</body>
+</html>
--- cours_presentielle/9.carte_postal/index.php
+++ cours_presentielle/9.carte_postal/index.php
@@ -4,19 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Carte Postal</title>
+    <title>Carte Postale</title>
+<link href="https://fonts.googleapis.com/css2?family=Ephesis&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="carte">
     <p class="carte_parisienne">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim magnam laborum a tempora, officiis libero explicabo sint ratione eius possimus, corporis debitis voluptates, odit voluptate provident id soluta! Nulla, enim.
+        <div class="texte">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia amet est vero vitae laborum soluta, sapiente quasi quam eaque, voluptatem quae tempora! Perferendis unde similique illum. Iure iusto quae iste?</p>
+            </div>
+            <div class="timbre">
+                <img src="img/timbre.png" alt="Timbre">
+                </div>
+                <img src="img/tampon.png" alt="Tampon">
+                <p class="tampon">
+                    <!--<script>
+                        var date = new Date;
+                        document.write(`${date.gatDate()}`,`/`, `${date.getMonth()})
+                    </script> -->
+                    <?php echo date('d/m/y'); ?>
+                </p>
+                <img src="img/obliteration.png" alt="Obliteration">
+                <div class="separateur"></div>
+                <div class="information">
+                    <p>Jean philipe Smet</p>
+                    <p>Cimetière de Lorient</p>
+                    <p>St Barthélemy</p>
+                    <p>France</p>
+                    </div>
+                    <div class="copyright">&copy; La Carte Prisienne Tel : 03.87.10.18.12</div>
 
+                    </div>
 
 
 
 
     </p>
     </div>
+    <body>
+        <table>
+            
+        </table>
+    </body>
         
 
 
