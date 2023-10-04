fee60a0cc7b20669f3baceb8842a722c6929fb0f
viktori57 <vrenezi.viktor57@gmail.com>
2023-10-04 16:07:05 +0200

    Commit all

---
+++ cours_presentielle/15.navbar2/navbar2.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <script src="jquery-3.7.1.min.js.js"></script>
+    <title>Name</title>
+    <link rel="stylesheet" href="navbar2.css">
+</head>
+<body>
+    
+    
+    <nav>
+        <div>
+            <box-icon name="world" color="#fff" size="30px"></box-icon>
+            <span>Mondial World</span>
+        </div>
+
+            <ul>
+                <li>About</li>
+                <li>Services</li>
+                <li>Testimonials</li>
+
+            </ul>
+        <div>
+
+                <box-icon name="github" type="logo" color="#fff" size="30px"></box-icon>
+                <box-icon id="menu" name="menu" color="#fff" size="30px"></box-icon>
+
+        </div>
+        
+    </nav>
+    <script>
+        $('#menu').click(function() {
+            if ($('nav ul').is(':visible')) {
+                $('nav ul').animate({
+                    opacity: 0,
+                }, 500, function() {
+                $('nav ul').hide()
+                })
+            } else {
+                $('nav ul').show()
+                $('nav ul').animate({
+                    opacity: 1,
+                    left: '50px', 
+            }, 500)
+
+            $('nav').css({
+                    'background-color': '#000D1A',
+                    'bottom': 0,
+        })
+    }
+    })
+
+    </script>
+  
+
+
+
+
+
+
+
+
+    
+    
+</body>
+</html>
