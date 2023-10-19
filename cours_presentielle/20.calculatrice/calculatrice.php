59904f92205098da3bd70034da352b69b3fa020f
viktori57 <vrenezi.viktor57@gmail.com>
2023-10-19 16:28:04 +0200

    Commit all

--- cours_presentielle/20.calculatrice/calculatrice.php
+++ cours_presentielle/20.calculatrice/calculatrice.php
@@ -2,54 +2,35 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Calculatrice</title>
+    <script src="../../scripts/jquery-3.7.1.min.js"></script>
+    <script src="script.js"></script>
     <link rel="stylesheet" href="style.css">
-    
+    <title>Calculatrice</title>
 </head>
 <body>
     <!-- Faire un formulaire en forme de calculatrice ou on poura calculer -->
-    <div class="calculator">
-        <input type="text" id="display" disabled>
-        <br>
-        <button onclick="AppendToDisplay('7')">7</button>
-        <button onclick="AppendToDisplay('8')">8</button>
-        <button onclick="AppendToDisplay('9')">9</button>
-        <button onclick="AppendToDisplay('*')">*</button>
-        <br>
-        <button onclick="AppendToDisplay('4')">4</button>
-        <button onclick="AppendToDisplay('5')">5</button>
-        <button onclick="AppendToDisplay('6')">6</button>
-        <button onclick="AppendToDisplay('-')">-</button>
-        <br>
-        <button onclick="AppendToDisplay('1')">1</button>
-        <button onclick="AppendToDisplay('2')">2</button>
-        <button onclick="AppendToDisplay('3')">3</button>
-        <button onclick="AppendToDisplay('+')">+</button>
-        <br>
-        
-        <button onclick="AppendToDisplay('0')">0</button>
-        <button onclick="CalculteResult()">=</button>
-        <button onclick="AppendToDisplay('/')">/</button>
-        <button onclick="ClearDisplay()">C</button>
-
-
-    </div>
-    <script>
-        function AppendToDisplay(value) {
-            document.getElementById("display").value += value;
-        }
-        function ClearDisplay() {
-            document.getElementById("display").value += "";
-        }
-        function CalculateResult() {
-            try {
-                document.getElementById("display").value =
-                eval(document.§getElementById("display").value);
-            } catch (e) {
-                document.getElementById("display").value = "Erreur";
-            }
-        }
-    </script>
+    <form action="" method="post">
+        <div>
+            <input type="text" id="input" readonly>
+        </div>
+        <div>
+            <button type="button" class="number">7</button>
+            <button type="button" class="number">8</button>
+            <button type="button" class="number">9</button>            
+            <button type="button" class="number">+</button>
+            <button type="button" class="number">4</button>
+            <button type="button" class="number">5</button>
+            <button type="button" class="number">6</button>            
+            <button type="button" class="number">-</button>
+            <button type="button" class="number">1</button>
+            <button type="button" class="number">2</button>
+            <button type="button" class="number">3</button>            
+            <button type="button" class="number">*</button>
+            <button type="button" class="number">/</button>            
+            <button type="button" class="number">0</button>            
+            <button type="button" class="clear">C</button>
+            <button type="button" class="egal">=</button>
+        </div>
         
     </form>
 </body>
