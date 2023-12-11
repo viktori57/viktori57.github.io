0d6316aee1929e3f889bbcd6860b1f4ee1f90f5d
viktori57 <vrenezi.viktor57@gmail.com>
2023-12-11 16:27:36 +0100

    Commit all

---
+++ cours_presentielle/28.premium/premiumteas.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../scss/main.css">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <title>PREMIUM TEAS</title>
+</head>
+<body>
+    <header>
+        <div class="title">
+            <div>
+                <h1>PREMIUM TEAS</h1>
+                <h3>UNIQUE TEA EXPERIENCE 🇨🇦</h3>
+            </div>
+            <div>
+                <box-icon type='logo' name='twitter'></box-icon>
+                <box-icon name='instagram' type='logo'></box-icon>
+                <box-icon name='facebook' type='logo'></box-icon>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">BLACK TEA</a></li>
+                <li><a href="">GREEN TEA</a></li>
+                <li><a href="">HERBAL</a></li>
+                <li><a href="">1LB BAGS</a></li>
+                <li><a href="">DECAF</a></li>
+                <li><a href="">COLLECTIONS</a></li>
+                <li><a href="">FALL TEA</a></li>
+                <li><a href="">GIFTS</a></li>
+                <li><a href="">MORE</a></li>
+                <li><a href="">SALE</a></li>
+                <li><input type="text" id="search" placeholder="Search"> <box-icon name='search' color='#c2c2c2'></box-icon></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="carrousel">
+
+    </div>
+</body>
+</html>
